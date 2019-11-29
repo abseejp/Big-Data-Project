a2f087d229df53662b55cf824519b0274f3f0f9e
Matthew Avallone <mva271@nyu.edu>
2019-11-29 13:57:45 -0500

    added profiling for INT columns

--- profiling.py
+++ profiling.py
@@ -18,77 +18,103 @@ from pyspark.sql.functions import *
 
 if __name__ == "__main__":
 
-    sc = SparkContext()
+	sc = SparkContext()
 
-    spark = SparkSession \
-        .builder \
-        .appName("profiling") \
-        .config("spark.some.config.option", "some-value") \
-        .getOrCreate()
+	spark = SparkSession \
+		.builder \
+		.appName("profiling") \
+		.config("spark.some.config.option", "some-value") \
+		.getOrCreate()
 
-    sqlContext = SQLContext(spark)
+	sqlContext = SQLContext(spark)
 
-    # get command-line arguments
-    inFile = sys.argv[1]
+	# get command-line arguments
+	inFile = sys.argv[1]
 
-    print ("Executing data profiling with input from " + inFile)
+	print ("Executing data profiling with input from " + inFile)
 
-    dataset = sqlContext.read.format('csv').options(header='true', inferschema='true', delimiter='\t', ignoreLeadingWhiteSpace='true', ignoreTrailingWhiteSpace='true').load(inFile)
-    dataset.createOrReplaceTempView("dataset")
-    sqlContext.cacheTable("dataset")
-    spark.sql("select COUNT(*) from dataset").show()
+	dataset = sqlContext.read.format('csv').options(header='true', inferschema='true', delimiter='\t', ignoreLeadingWhiteSpace='true', ignoreTrailingWhiteSpace='true').load(inFile)
+	dataset.createOrReplaceTempView("dataset")
+	sqlContext.cacheTable("dataset")
 
-    attributes = dataset.columns
-    dataset.printSchema()
+	attributes = dataset.columns
+	dataset.printSchema()
 
-    #================= DataFrame Operations ==================
+	#================= DataFrame Operations ==================
 
-    # Count all rows
-    count = dataset.count()
+	# Attribute Data Type array
+	attribute_types = dict(dataset.dtypes)
 
-    # Count distinct values
-    distinct_count = dataset.distinct().count()
-
-    # Attribute Data Type array
-    attribute_types = dict(dataset.dtypes)
-
-    # Frequent Itemsets
-    # need to retrieve sets of size 2, 3, and 4
-    itemSets = dataset.freqItems(dataset.columns)
+	# Frequent Itemsets
+	# need to retrieve sets of size 2, 3, and 4
+	itemSets = dataset.freqItems(dataset.columns)
 
 
 
-    #================= Spark SQL Operations ===================
+	#================= Spark SQL Operations ===================
 
-    # for INTEGER type attributes, compute max, min, mean, stdev
-    numData = dict()
-    for attr in attributes:
+	# for INTEGER type attributes, compute max, min, mean, stdev
+	numData = dict()
+	strData = dict()
+	prim_key = []
+	# Loop through every column
+	for attr in attributes:
+		print(attr)
+		
+		# Count all values for a column
+		num_col_values = dataset.select(attr).count()
+		print("num_col_values:", num_col_values)
+		
+		# Count number of distinct values
+		num_distinct_col_values = dataset.agg(countDistinct(col(attr)).alias("count_distinct")).collect()[0]["count_distinct"]
+		print("num_distinct_col_values:",num_distinct_col_values)
+		
+		# Finding potential primary keys
+		if(num_distinct_col_values >= num_col_values*0.9):
+			prim_key.append(attr)
 
-        # Finding potential primary keys
-        num_distinct_col_values = dataset.agg(countDistinct(col(attr)).alias("count"))
-        prim_key = []
-        if(num_distinct_col_values >= count*0.9):
-            prim_key.append(attr)
+		# Find data types of all values in the column
+		# def findType(x):
+		# 	return str(type(x))
+
+		# udftype = udf(findType, StringType())
+		# column_types = dataset.select(attr).rdd.map(lambda x: (x, udftype(x))).toDF()
+		# column_types.show()
+		# type_counts = column_types.groupBy("_2").count().alias("count_types")
+		# type_counts.show()
+		dtype = attribute_types[attr]
+		
+		if(dtype == 'int'):
+
+			# query = """select max('""" + attr + """') as maxAttr, min('""" + attr + """') as minAttr, mean('""" + attr + """') as meanAttr,  std('""" + attr + """') as stdAttr from dataset"""
+			# # result = spark.sql(query).select(format_string('%.2f,%.2f,%.2f,%.2f', result.maxAttr, result.minAttr, result.meanAttr, result.stdAttr)).write.save("result.json",format="json")
+			# result = spark.sql(query)
+			# result.createOrReplaceTempView("result")
+			# # result = result.select(format_string('%.2f,%.2f,%.2f,%.2f', result.maxAttr, result.minAttr, result.meanAttr, result.stdAttr))
+			# spark.sql("select * from result").show()
+			
+			stats = dataset.agg(max(col(attr)).alias("max"), min(col(attr)).alias("min"), mean(col(attr)).alias("mean"), stddev(col(attr)).alias("stddev"))
+			col_max = stats.collect()[0]["max"]
+			col_min = stats.collect()[0]["min"]
+			col_mean = stats.collect()[0]["mean"]
+			col_stddev = stats.collect()[0]["stddev"]
+
+			numData[attr] = [col_max, col_min, col_mean, col_stddev]
+
+		elif(dtype == 'date'):
+			# Format date attributes to same structure
+			continue
+		
+		elif(dtype == 'string'):
+			# Find max and min string length
+			continue
+			win = Window.orderBy(length(attr).desc())
+			win_asc = Window.orderBy(length(attr).asc)
+			dataset.select(attr).withColumn("str_len",length(attr)).withColumn("row_max", row_number().over(win_desc)).withColumn("row_min",row_number().over(win_asc)).filter('row_max <= 5 || row_min <= 5').show()
 
 
-        # Agrregation on INTEGER column types
-        dtype = attribute_types(attr)
-        
-        if(dtype == 'int'):
 
-            query = """select max('""" + attr + """') as maxAttr, min('""" + attr + """') as minAttr, mean('""" + attr + """') as meanAttr,  std('""" + attr + """') as stdAttr from dataset"""
-            # result = spark.sql(query).select(format_string('%.2f,%.2f,%.2f,%.2f', result.maxAttr, result.minAttr, result.meanAttr, result.stdAttr)).write.save("result.json",format="json")
-            result = spark.sql(query)
-            result.createOrReplaceTempView("result")
-            # result = result.select(format_string('%.2f,%.2f,%.2f,%.2f', result.maxAttr, result.minAttr, result.meanAttr, result.stdAttr))
-            spark.sql("select * from result").show()
-            
-            # numData[attr] = [result.maxAttr.val, result.minAttr.val, result.meanAttr.val, result.stdAttr.val]
-            # print(attr, [result.maxAttr.val, result.minAttr.val, result.meanAttr.val, result.stdAttr.val])
+	#================== Saving as JSON file =====================
 
-
-
-    #================== Saving as JSON file =====================
-
-    # need to verify that this works
-    # df.write.format('json').save(path=os.getcwd())
+	# need to verify that this works
+	# df.write.format('json').save(path=os.getcwd())
