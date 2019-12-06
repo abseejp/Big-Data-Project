61cf9de0c3f9fa4eb73345cfd929f8679ee45796
Matthew Avallone <mva271@nyu.edu>
2019-12-06 12:04:20 -0500

    updated comments

--- task1/run_task1.sh
+++ task1/run_task1.sh
@@ -1,17 +1,35 @@
-# TO USE:
-# Change scratch location USER to individual using it
+# Setup:
+# Change scratch location NETID to individual using it
+# Make sure to update location of file list
 # File was run on NYU DUMBO HPC
 
+# To Use:
+# Comment out the line labeled "2" and uncomment the two lines labeled "1". Running these will get all of the files and put them on the Hadoop server
+# Comment out the two lines labeled "1" and uncomment the line labeled "2". Running this will loop through a list of files and run them with spark.
+
+# Note: once the datasets are put on hadoop server, they can be physically deleted from scratch
+
+
+
+########## 1 ##########
 # /usr/bin/hadoop fs -get "/user/hm74/NYCOpenData" "/scratch/mva271"
+#######################
 
 input="/scratch/mva271/NYCOpenData/files.txt"
 while IFS= read -r line
 do
+	########## 1 ##########
 	# /usr/bin/hadoop fs -put "$line"
+	#######################
+
 	# echo "$line"
 	# read -p 'skip this file (y/n)?: ' skipvar
 	# if [ "$skipvar" = "n" ]
 	# then
+
+	########## 2 ##########
 	spark-submit --conf spark.pyspark.python=$PYSPARK_PYTHON profiling.py "$line"
+    #######################
+
     # fi
 done < "$input"
